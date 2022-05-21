8aa552ed457fadb36ce6e4455f097bc76cef4328
DworshiFH <andreas.dworscak@stud.fh-campuswien.ac.at>
2022-05-21 12:40:32 +0200

    Hello World

--- src/at/ac/fhcampuswien/HideThePain/EightPuzzle/Main.java
+++ src/at/ac/fhcampuswien/HideThePain/EightPuzzle/Main.java
@@ -1,9 +1,11 @@
 package at.ac.fhcampuswien.HideThePain.EightPuzzle;
 
 public class Main {
-    
+
 
     public static void main(String[] args) {
+
+        System.out.println("Puzzle solved, thank you for choosing \"EightPuzzle\", by \"Hide The Pain\"");
 
     }
 }
